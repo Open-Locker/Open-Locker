19826eea8f52e557947311cd9ba4bf710981b9f2
sdreher <sarah@merona.de>
2025-06-01 14:27:26 +0200

    enables admin creation from filament backend with automatic password reset mail

--- locker-backend/app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ locker-backend/app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -4,8 +4,10 @@ namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
+use Filament\Facades\Filament;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
 
 class CreateUser extends CreateRecord
 {
@@ -16,8 +18,19 @@ class CreateUser extends CreateRecord
         $data['password'] = bcrypt('test');
         $user = static::getModel()::create($data);
 
+//        $notification->url = filament()->getPanel('admin')->getResetPasswordUrl($token, $user);
+        $user->is_admin_since = now();
         $user->email_verified_at = now();
         $user->save();
         return $user;
+    }
+
+    protected function afterCreate(): void
+    {
+        $user = $this->record;
+        $token = app('auth.password.broker')->createToken($user);
+        $notification = new \Filament\Notifications\Auth\ResetPassword($token);
+        $notification->url = \Filament\Facades\Filament::getResetPasswordUrl($token, $user);
+        $user->notify($notification);
     }
 }
