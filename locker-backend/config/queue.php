eb2940535281eec32876db942e5d6d479de39215
Merlin Hofmann | Merona <merlin@merona.de>
2025-12-29 22:20:54 +0100

    fix(queue): use single default queue name instead of comma list

--- locker-backend/config/queue.php
+++ locker-backend/config/queue.php
@@ -38,7 +38,7 @@ return [
             'driver' => 'database',
             'connection' => env('DB_QUEUE_CONNECTION'),
             'table' => env('DB_QUEUE_TABLE', 'jobs'),
-            'queue' => env('DB_QUEUE', 'default,events'),
+            'queue' => env('DB_QUEUE', 'default'),
             'retry_after' => (int) env('DB_QUEUE_RETRY_AFTER', 90),
             'after_commit' => false,
         ],
@@ -66,7 +66,7 @@ return [
         'redis' => [
             'driver' => 'redis',
             'connection' => env('REDIS_QUEUE_CONNECTION', 'default'),
-            'queue' => env('REDIS_QUEUE', 'default,events'),
+            'queue' => env('REDIS_QUEUE', 'default'),
             'retry_after' => (int) env('REDIS_QUEUE_RETRY_AFTER', 90),
             'block_for' => null,
             'after_commit' => false,
