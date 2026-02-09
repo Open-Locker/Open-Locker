24803fea48e8c8c7eca7e9979ce7548a9bb7abaa
{merona-sarah} <{sarah@merona.de}>
2026-02-09 17:47:22 +0100

    adds more partner logos

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,16 +1,28 @@
 export const LINKS = {
 	githubRepo: 'https://github.com/Open-Locker/Open-Locker',
 	discordInvite: 'https://discord.gg/rZ74RYKN3H',
-	partners: [
+	funders: [
 		{
-			name: 'Smart City Hameln-Pyrmont',
-			url: 'https://mitwirkportal.de/informieren/',
-			logoSrc: '/ha-py-smart-city-logo (1).svg',
+			name: 'BMWSB',
+			url: 'https://www.bmwsb.bund.de/',
+			logoSrc: '/BMWSB Logo.png',
+		},
+		{
+			name: 'KfW',
+			url: 'https://www.kfw.de/',
+			logoSrc: '/KfW_Logo.JPG',
 		},
 		{
 			name: 'Landkreis Hameln-Pyrmont',
 			url: 'https://www.hameln-pyrmont.de/',
-			logoSrc: '/logo_landkreis-hameln-pyrmont (1).png',
+			logoSrc: '/Landkreis_Logo.gif',
+		},
+	],
+	partners: [
+		{
+			name: 'Smart City Hameln-Pyrmont',
+			url: 'https://mitwirkportal.de/informieren/',
+			logoSrc: '/Ha-Py-Smart City-Landkreis Hameln-Pyrmont-rgb (4).jpg',
 		},
 		{
 			name: 'Merona',
