739d90b66a5e2b3c12d9c0eb00aaff090bafe6fe
Merlin Hofmann | Merona <merlin@merona.de>
2026-02-28 14:37:21 +0000

    deletes legacy flutter app

--- mobile-app-legacy-flutter/README.md
+++
@@ -1,3 +0,0 @@
-# locker_app
-
-A new Flutter project.
--- mobile-app-legacy-flutter/android/app/src/main/kotlin/com/example/locker_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.locker_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
