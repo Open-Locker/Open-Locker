305cc07b87e91a23ff31f9f74afb570e894e9523
Simon Engelhardtm <smn@nglhrdt.de>
2026-05-23 17:43:33 +0200

    feat(logging): add log volume to docker-compose and update logger file path

--- locker-client/src/helper/logger.ts
+++ locker-client/src/helper/logger.ts
@@ -5,9 +5,12 @@ dotenv.config();
 
 export const logger = winston.createLogger({
     level: process.env.LOG_LEVEL || 'info',
-    format: winston.format.json(),
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+    ),
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename: 'app.log' })
+        new winston.transports.File({ filename: '/log/app.log' })
     ]
 });
